b0fa3bcd0d53b56068f350682e82b20f570ae5b3
cicelyhb <31481716+cicelyhb@users.noreply.github.com>
2017-09-06 12:16:26 -0400

    Added columns bLetter and LetterType to table territory

--- BUILD.sql
+++ BUILD.sql
@@ -1,1 +1,18 @@
+ALTER TABLE ministryapp.territory ADD bLetter tinyint;
 
+SET SQL_SAFE_UPDATES = 0; 
+UPDATE ministryapp.territory
+SET bLetter = 0;
+SET SQL_SAFE_UPDATES = 0; 
+
+ALTER TABLE ministryapp.territory MODIFY COLUMN bLetter tinyint NOT NULL;
+
+
+ALTER TABLE ministryapp.territory ADD LetterType VARCHAR(10);
+
+SET SQL_SAFE_UPDATES = 0; 
+UPDATE ministryapp.territory
+SET LetterType = 'LNS';
+SET SQL_SAFE_UPDATES = 0; 
+
+ALTER TABLE ministryapp.territory MODIFY COLUMN LetterType VARCHAR(10) NOT NULL;
